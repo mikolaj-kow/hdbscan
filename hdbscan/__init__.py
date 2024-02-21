55c0d39331ba92522b629a0ed32baa9904d7b197
Mikołaj Kowalski <cmosqt@gmail.com>
2024-02-21 18:43:05 +0100

    ray validity

--- hdbscan/__init__.py
+++ hdbscan/__init__.py
@@ -1,6 +1,6 @@
 from .hdbscan_ import HDBSCAN, hdbscan
 from .robust_single_linkage_ import RobustSingleLinkage, robust_single_linkage
-from .validity import validity_index
+from .validity import validity_index_ray
 from .prediction import (approximate_predict,
                          membership_vector,
                          all_points_membership_vectors,
